7bfad16bb70dfb4456830e97f94d4eedeb0d7d9d
ConnorBradshaw <connorbradshaw10@gmail.com>
2019-03-06 14:57:06 -0500

    Adding / get

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use('/group', group);
 app.use('/user', user);
 app.use('/location', location);
 
+app.get('/', (req,res) => {
+	res.send("Hello");
+});
 app.listen(4000, () => {
 	console.log("Listening on port 4000");
 });
